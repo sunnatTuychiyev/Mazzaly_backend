61eb4ebfe42136307e643ab7860861d3259f8d33
Ubuntu <ubuntu@ip-172-31-81-46.ec2.internal>
2025-07-07 18:30:05 +0000

    fix OTP

--- Mazzaly_backend/settings.py
+++ Mazzaly_backend/settings.py
@@ -174,17 +174,31 @@ USE_TZ = True
 # Default to console backend for development. If EMAIL_HOST_USER and
 # EMAIL_HOST_PASSWORD are provided, use SMTP settings for sending real
 # emails (e.g. via Gmail).
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
-if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
-    EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-    EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+
+#if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD:
+ #   EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+ #   EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+ #   EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+ #   EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+ #   DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+#else:
+ #   EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+
+
+
+
 SPOONACULAR_API_KEY = config('SPOONACULAR_API_KEY', default='')
 TELEGRAM_BOT_TOKEN = config('TELEGRAM_BOT_TOKEN', default='')
 
