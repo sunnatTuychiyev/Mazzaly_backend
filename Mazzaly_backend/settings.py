a212c65c33a74554d0901683869214cda6cee923
Tuychiyev Sunnatillo <151430152+sunnatTuychiyev@users.noreply.github.com>
2025-07-07 17:04:58 +0500

    Update settings.py

--- Mazzaly_backend/settings.py
+++ Mazzaly_backend/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sslserver',
-    'django.contrib.sites',
+    'django.contrib.sites',          # Sites (kerak bo‘lsa)
     'rest_framework',
     'rest_framework_simplejwt',
     'drf_yasg',
