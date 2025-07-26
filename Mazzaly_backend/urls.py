c9ab13a90091f7dd742e4326efbb2ff4645355cb
Tuychiyev Sunnatillo <151430152+sunnatTuychiyev@users.noreply.github.com>
2025-07-26 22:24:17 +0500

    Add SimpleJWT token endpoints

--- Mazzaly_backend/urls.py
+++ Mazzaly_backend/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 admin.site.site_header = "Mazzaly Administration"
 admin.site.site_title = "Mazzaly Admin Portal"
@@ -23,6 +27,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/', include('account.urls')),     # Auth, user, Google OAuth va h.k.
     path('api/', include('recipes.urls')),     # Recipes, ingredients, meal plan va h.k.
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
